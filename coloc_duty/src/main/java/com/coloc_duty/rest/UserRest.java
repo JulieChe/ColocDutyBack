fc575fe492101c2e74ad14a2306ec92524cd2127
VicPARE <victor.pare@outlook.fr>
2021-03-12 14:59:53 +0100

    Ajout de l'API de création de User, et du test de user existant dans la base.

--- coloc_duty/src/main/java/com/coloc_duty/rest/UserRest.java
+++ coloc_duty/src/main/java/com/coloc_duty/rest/UserRest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coloc_duty.entities.User;
@@ -31,5 +33,43 @@ public class UserRest {
 		
 		return (long) 0;
 	}
+	
+	
+	@GetMapping("/users")
+	public List<User> getAllUsers() {
+		return (List<User>) userRepo.findAll();
+
+	}
+
+	@PostMapping("/saveuser")
+	public User saveUser(User user) {
+		return userRepo.save(user);
+
+	}
+
+	@GetMapping("/signin")
+	public String subscribed(@RequestBody User user) {
+		List<User> l = getAllUsers();
+		String p = null;
+		int i = 0;
+		System.out.println("le login du user à ajouter est : " + user.getLogin());
+		System.out.println("le mail du user à ajouter est : " + user.getEmail());
+		while (i < l.size() && p == null) {
+			if (l.get(i).getLogin().equals(user.getLogin())) {
+				p = "errorLogin";
+			}
+			if (l.get(i).getEmail().equals(user.getEmail())) {
+				p = "errorEmail";
+			}
+			if (l.get(i).getLogin().equals(user.getLogin()) && l.get(i).getEmail().equals(user.getEmail())) {
+				p="errorBoth";
+				
+			}
+			i++;
+			System.out.println("Le login du user : " + i + " est : " + l.get(i - 1).getLogin());
+		}
+		return p;
+	}
+	
 
 }
