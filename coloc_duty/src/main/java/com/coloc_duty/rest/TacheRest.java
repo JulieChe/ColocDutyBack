bc781dddab099d9cf61c6bb0d6b0fa2380b8c487
thierry308 <thierrysalmon9@gmail.com>
2021-03-16 17:13:00 +0100

    Creation Tache back

--- coloc_duty/src/main/java/com/coloc_duty/rest/TacheRest.java
+++ coloc_duty/src/main/java/com/coloc_duty/rest/TacheRest.java
@@ -1,9 +1,18 @@
 package com.coloc_duty.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coloc_duty.entities.Adresse;
+import com.coloc_duty.entities.Coloc;
+import com.coloc_duty.entities.Tache;
+import com.coloc_duty.entities.User;
 import com.coloc_duty.repository.TacheRepository;
 
 @RestController
@@ -13,6 +22,12 @@ public class TacheRest {
 	
 	@Autowired
 	private TacheRepository tacheRepo;
+	
+	@PostMapping("/savetache")
+	public Tache saveTache(@RequestBody Tache tache) {
+		Tache t = tacheRepo.save(tache);
+		return t;
+	}
 	
 	
 	
